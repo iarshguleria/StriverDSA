a672734b6c861a7a9a25ecb2f6c59828ccd9dc77
Arsh Guleria <arshguleria1612@gmail.com>
2023-11-29 15:54:35 +0530

    New

---
+++ Step1: Learn the Basics/Patterns/Pattern1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void pattern1(int n){
+    for (int i = 0; i < n; i++)
+    {
+       for (int i = 0; i < n; i++)
+       {
+            cout<<"* ";
+       }
+       cout<<endl;
+    }
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    pattern1(n);
+
+    return 0;
+}
--- Step1: Learn the Basics/Patterns/Pattern7.cpp
+++ Step1: Learn the Basics/Patterns/Pattern7.cpp
@@ -4,15 +4,15 @@ using namespace std;
 void pattern1(int n){
     for (int i = 0; i <n; i++)
     {
-        for (int j = 0; j <i; j++)
+        for (int j = 0; j <n-i+1; j++)
         {
             cout<<" ";
         }
-        for (int j = 0; j <2*n -(2*i + 1); j++)
+        for (int j = 0; j <i*2+1; j++)
         {
             cout<<"*";
         }
-        for (int j = 0; j <i; j++)
+        for (int j = 0; j <n-i+1; j++)
         {
             cout<<" ";
         }
